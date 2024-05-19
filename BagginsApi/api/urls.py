574bb26cba0e7f2bae5b4e796f116eb660204eab
4218 Пчеленко Е.Ю <e.pchelenko@gmail.com>
2024-05-20 00:38:32 +0300

    added uploading files 2.0

--- BagginsApi/api/urls.py
+++ BagginsApi/api/urls.py
@@ -5,7 +5,7 @@ from api.views import FileUploadView
 from .serializers import CookieTokenObtainPairView, CookieTokenRefreshView
 urlpatterns = [
     path('', include('djoser.urls')),
-    path('upload/', FileUploadView.as_view())
+    path('upload/', FileUploadView.as_view()),
     path('auth/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
 ]
